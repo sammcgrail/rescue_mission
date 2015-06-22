6bf35b6c1648edabfa6c71f07c263cc38afa6148
sammcgrail <sammcgrail@gmail.com>
2015-06-22 16:13:03 -0400

    show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :questions, only: [:index]
+  resources :questions, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/features/user_views_question_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'user views a questions details', %Q{
+  As an authenticated user
+  I want to be able to click on a question in the question index page
+  So I can see the details for that question in question
+} do
+  # Acceptance Criteria
+  # - I must be able to get to this page from the questions index
+  # - I must see the question's title
+  # - I must see the question's description
+
+  scenario 'user visits question index' do
+    user = FactoryGirl.create(:user)
+    question = Question.create!(title:"Test Question", body: "STUFF", user_id: user.id, created_at: "Mon, 22 Jun 2015 17:26:19 UTC +00:00")
+    visit questions_path
+    click_on question.title
+    expect(page).to have_content(question.title)
+    expect(page).to have_content(question.body)
+    expect(page).to have_content("Question Details:")
+  end
+
+  # scenario 'unauthenticated user attempts to sign out' do
+  #   visit '/'
+  #   expect(page).to_not have_content('Sign Out')
+  # end
+end
